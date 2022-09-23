e1cfa14c92016e869127c60fa0dff6126b84d3ed
Shivansh Pandey <53964760+shivansh1012@users.noreply.github.com>
2022-09-23 20:26:32 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const PORT = process.env.SERVER_PORT || 5000
 
 console.log('Connecting to DB...')
 
-mongoose.connect(process.env.MONGO_ATLAS_URL, {
+mongoose.connect(process.env.MONGO_ATLAS_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }, (err) => {
