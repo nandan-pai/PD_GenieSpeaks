0ba0536b9c8b12b92e920f79cd3f824a40218049
Shivansh Pandey <shivansh.spandey@gmail.com>
2022-09-23 21:49:16 +0530

    Removed env from webapp

--- webapp/src/components/ProductList/ProductList.jsx
+++ webapp/src/components/ProductList/ProductList.jsx
@@ -9,13 +9,9 @@ import { ApiBaseUrl } from "../../config";
 const ProductList = ({ searchQuery, setSearchQuery }) => {
 	const [productList, setProductList] = useState([]);
 	const getProductList = useCallback(() => {
-		console.log(`${ApiBaseUrl}/prod/search?query=${searchQuery}`)
-		console.log(process.env)
 		axios.get(`${ApiBaseUrl}/prod/search?query=${searchQuery}`).then((res) => {
 			// console.log(res.data.productList)
-			if(res.data.productList !== undefined){
-				setProductList(res.data.productList);
-			}
+			setProductList(res.data.productList);
 		});
 	}, [searchQuery]);
 
--- webapp/src/config.js
+++ webapp/src/config.js
@@ -1,1 +1,6 @@
-export const ApiBaseUrl = process.env.REACT_APP_NODE_ENV === 'production' ? process.env.REACT_APP_API_URI : 'http://localhost:5000/api'
+// env
+// REACT_APP_NODE_ENV=production
+// REACT_APP_API_URI=https://gsserver.azurewebsites.net/api
+
+// export const ApiBaseUrl = process.env.REACT_APP_NODE_ENV === 'production' ? process.env.REACT_APP_API_URI : 'http://localhost:5000/api'
+export const ApiBaseUrl = 'https://gsserver.azurewebsites.net/api'
