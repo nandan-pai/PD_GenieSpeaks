e206c3176c163c5c6e2efa5d5cadd9da9b840fa8
Shivansh Pandey <shivansh.spandey@gmail.com>
2022-09-23 19:27:32 +0530

    Forgot this

--- webapp/src/config.js
+++ webapp/src/config.js
@@ -1,1 +1,1 @@
-export const ApiBaseUrl = process.env.NODE_ENV === 'development' ? 'https://gsserver.azurewebsites.net/api' : process.env.apiuri
+export const ApiBaseUrl = process.env.NODE_ENV === 'development' ? 'https://gsserver.azurewebsites.net/api' : process.env.API_URI
