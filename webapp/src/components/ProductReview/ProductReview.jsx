b5f7d9fa8805ca1367125783a45c09a42b0f2534
Gaurav Gupta <guptagaurav035@gmail.com>
2023-02-22 11:09:35 +0530

    Fixed star rating and added form submit to review page

--- webapp/src/components/ProductReview/ProductReview.jsx
+++ webapp/src/components/ProductReview/ProductReview.jsx
@@ -21,11 +21,14 @@ import { FaChevronLeft } from "react-icons/fa";
 import InputReviewStars from "./InputReviewStars";
 
 const ProductReview = (props) => {
-
 	const [productInfo, setProductInfo] = useState({});
 	const [isLoading, setIsLoading] = useState(true);
-
 	const [reviewStar, setReviewStar] = useState(0);
+
+	const [formValues, setFormValues] = useState({
+		heading: "",
+		review: "",
+	});
 
 	const navigate = useNavigate();
 
@@ -43,6 +46,34 @@ const ProductReview = (props) => {
 
 	const handleBack = () => {
 		navigate(`/product/${id}`);
+	};
+
+	const handleHeadingChange = (e) => {
+		e.persist();
+
+		setFormValues((values) => ({
+			...values,
+			heading: e.target.value,
+		}));
+	};
+
+	const handleReviewChange = (e) => {
+		e.persist();
+
+		setFormValues((values) => ({
+			...values,
+			review: e.target.value,
+		}));
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		if (formValues.heading !== "" && formValues.review !== "") {
+			console.log(formValues);
+		} else {
+			console.error("Form not filled completely");
+		}
 	};
 
 	if (isLoading) {
@@ -67,57 +98,73 @@ const ProductReview = (props) => {
 					setSearchQuery={props.setSearchQuery}
 					setFilter={props.setFilter}
 				/>
-				<Box mx={80} p={5}>
-					<HStack
-						mb={5}
-						_hover={{ textDecoration: "underline", cursor: "pointer" }}
-						onClick={handleBack}
-						w='80px'
-					>
-						<Icon as={FaChevronLeft} />
-						<Text fontWeight='semibold'>Back</Text>
-					</HStack>
-					<VStack mx={30} align='start' spacing={5}>
-						<Heading size='md'>Write a Review</Heading>
-						<HStack align='start' mt={5} spacing={5}>
-							<Image src={productInfo.images[0]} w='150px' h='150px' />
-							<Text>{productInfo.title}</Text>
+				<form onSubmit={handleSubmit}>
+					<Box mx={80} p={5}>
+						<HStack
+							mb={5}
+							_hover={{ textDecoration: "underline", cursor: "pointer" }}
+							onClick={handleBack}
+							w='80px'
+						>
+							<Icon as={FaChevronLeft} />
+							<Text fontWeight='semibold'>Back</Text>
 						</HStack>
+						<VStack mx={30} align='start' spacing={5}>
+							<Heading size='md'>Write a Review</Heading>
+							<HStack align='start' mt={5} spacing={5}>
+								<Image src={productInfo.images[0]} w='150px' h='150px' />
+								<Text>{productInfo.title}</Text>
+							</HStack>
 
-						<Divider orientation='horizontal' />
+							<Divider orientation='horizontal' />
 
-						<Heading size='sm'>Overall Rating</Heading>
-						<InputReviewStars reviewStar={reviewStar} setReviewStar={setReviewStar}/>
+							<Heading size='sm'>Overall Rating</Heading>
+							<InputReviewStars
+								reviewStar={reviewStar}
+								setReviewStar={setReviewStar}
+							/>
 
-						<Divider orientation='horizontal' mb={5} mt={5} />
+							<Divider orientation='horizontal' mb={5} mt={5} />
 
-						<Heading size='sm'>Add a Heading</Heading>
-						<Input
-							type='text'
-							_hover={{ borderColor: "blue" }}
-							placeholder='What should everyone know?'
-						/>
+							<Heading size='sm'>Add a Heading</Heading>
+							<Input
+								type='text'
+								name='heading'
+								placeholder='What should everyone know?'
+								value={formValues.heading}
+								onChange={handleHeadingChange}
+								_hover={{ borderColor: "blue" }}
+							/>
 
-						<Divider orientation='horizontal' mb={5} mt={5} />
+							<Divider orientation='horizontal' mb={5} mt={5} />
 
-						<Heading size='sm'>Add Product Review</Heading>
-						<Textarea
-							type='text'
-							h='150px'
-							_hover={{ borderColor: "blue" }}
-							resize='vertical'
-							placeholder='What did you use the product for? What did you like or dislike?'
-						/>
+							<Heading size='sm'>Add Product Review</Heading>
+							<Textarea
+								type='text'
+								name='review'
+								h='150px'
+								resize='vertical'
+								placeholder='What did you use the product for? What did you like or dislike?'
+								value={formValues.review}
+								onChange={handleReviewChange}
+								_hover={{ borderColor: "blue" }}
+							/>
 
-						<Divider orientation='horizontal' mb={5} mt={5} />
+							<Divider orientation='horizontal' mb={5} mt={5} />
 
-						<div className='postBtn'>
-							<Button type='submit' size='md' bgColor='gray.100' color='white'>
-								Post
-							</Button>
-						</div>
-					</VStack>
-				</Box>
+							<div className='postBtn'>
+								<Button
+									type='submit'
+									size='md'
+									bgColor='gray.100'
+									color='white'
+								>
+									Post
+								</Button>
+							</div>
+						</VStack>
+					</Box>
+				</form>
 			</>
 		);
 	}
--- webapp/src/components/ProductReview/Star.jsx
+++ webapp/src/components/ProductReview/Star.jsx
@@ -1,23 +1,19 @@
 import { FaRegStar, FaStar } from "react-icons/fa";
-import {
-	Icon,
-	useColorMode,
-} from "@chakra-ui/react";
+import { Icon, useColorMode } from "@chakra-ui/react";
 
-export default function Star({index, reviewStar, setReviewStar}) {
-    const { colorMode } = useColorMode();
-    const isDark = colorMode === "dark";
-  return (
-    <Icon
-        as={reviewStar ? FaStar : FaRegStar}
-        mr='2px'
-        fontSize='30px'
-        color={
-            reviewStar>=index ? "orange" : isDark ? "white" : "black"
-        }
-        key={index}
-        _hover={{ cursor: "pointer", color: "orange" }}
-        onClick={() => setReviewStar(index)}
-    />
-  )
+export default function Star({ index, reviewStar, setReviewStar }) {
+	const { colorMode } = useColorMode();
+	const isDark = colorMode === "dark";
+
+	return (
+		<Icon
+			as={reviewStar >= index ? FaStar : FaRegStar}
+			mr='2px'
+			fontSize='30px'
+			color={reviewStar >= index ? "orange" : isDark ? "white" : "black"}
+			key={index}
+			_hover={{ cursor: "pointer", color: "orange" }}
+			onClick={() => setReviewStar(index)}
+		/>
+	);
 }
