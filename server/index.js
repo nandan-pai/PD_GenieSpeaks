0f93314b0f602d878437085ef85af685fc68d084
Nandan Pai <nandanpai2k1@gmail.com>
2023-03-01 15:27:10 +0530

    creating convinient directory by deleting old files

--- server/index.js
+++
@@ -1,6 +0,0 @@
-const app = require("./server.js");
-require('dotenv').config()
-
-const PORT = process.env.SERVER_PORT || 5000;
-
-app.listen(PORT, () => console.log(`Server running on PORT : ${PORT}`));
--- webapp/src/config.js
+++
@@ -1,8 +0,0 @@
-// env
-// REACT_APP_NODE_ENV=production
-// REACT_APP_API_URI=https://gsserver.azurewebsites.net/api
-
-// export const ApiBaseUrl = process.env.REACT_APP_NODE_ENV === 'production' ? process.env.REACT_APP_API_URI : 'http://localhost:5000/api'
-export const LocalServerUrl = "http://localhost:5000/api";
-export const GlobalServerUrl = "https://devserves.azurewebsites.net/api";
-export const ApiBaseUrl = GlobalServerUrl;
